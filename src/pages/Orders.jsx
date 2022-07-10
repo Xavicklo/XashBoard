ff2b3572c3e3c92c5344109de05547db020c187f
Xavicklo <Andy286460@gmail.com>
2022-07-10 23:44:27 +0800

    Slicing

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -15,10 +15,12 @@ import {
 } from "@syncfusion/ej2-react-grids";
 import { ordersData, contextMenuItems, ordersGrid } from "../data/dummy";
 import { Header } from "../components";
+import { useStateContext } from "../contexts/ContextProvider";
 
 const Orders = () => {
+  const { currentMode, currentColor } = useStateContext();
   return (
-    <div className="m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl">
+    <div className="m-2 md:m-10 p-2 md:p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl">
       <Header category="Page" title="Orders" />
       <GridComponent
         id="gridcomp"
