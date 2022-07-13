6bcb7f2eca51bb8e44fe72272afd8624a23d2249
Xavicklo <Andy286460@gmail.com>
2022-07-13 23:53:59 +0800

    Slicing

--- src/pages/ColorPicker.jsx
+++ src/pages/ColorPicker.jsx
@@ -9,7 +9,7 @@ const change = (args) => {
 
 const ColorPicker = () => {
   return (
-    <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
+    <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl">
       <Header category="App" title="Color Picker" />
       <div className="text-center">
         <div id="preview"></div>
