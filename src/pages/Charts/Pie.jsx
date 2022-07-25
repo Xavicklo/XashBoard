f0437f40f34e035223f880bc773b1dd49c8ed5d7
Xavicklo <Andy286460@gmail.com>
2022-07-25 23:54:58 +0800

    Add Pie Chart

--- src/pages/Charts/Pie.jsx
+++ src/pages/Charts/Pie.jsx
@@ -1,12 +1,20 @@
 import React from "react";
-import { Header } from "../../components";
 
-const Pie = () => {
-  return (
-    <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl">
-      <Header category="App" title="Pie Chart" />
+import { pieChartData } from "../../data/dummy";
+import { Header, Pie as PieChart } from "../../components";
+
+const Pie = () => (
+  <div className="m-4 md:m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl">
+    <Header category="Pie" title="Project Cost Breakdown" />
+    <div className="w-full">
+      <PieChart
+        id="chart-pie"
+        data={pieChartData}
+        legendVisiblity
+        height="full"
+      />
     </div>
-  );
-};
+  </div>
+);
 
 export default Pie;
