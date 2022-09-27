efc8209a6ad9271f3e2bdf7e47b0dd2e1095e1ec
Xavicklo <Andy286460@gmail.com>
2022-09-27 20:43:21 +0800

    New Features

--- src/pages/Charts/Pie.jsx
+++ src/pages/Charts/Pie.jsx
@@ -3,18 +3,24 @@ import React from "react";
 import { pieChartData } from "../../data/dummy";
 import { Header, Pie as PieChart } from "../../components";
 
-const Pie = () => (
-  <div className="m-4 md:m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl">
-    <Header category="Pie" title="Project Cost Breakdown" />
-    <div className="w-full">
-      <PieChart
-        id="chart-pie"
-        data={pieChartData}
-        legendVisiblity
-        height="full"
-      />
+import { useStateContext } from "../../contexts/ContextProvider";
+
+const Pie = () => {
+  const { currentMode } = useStateContext();
+  return (
+    <div className="m-4 md:m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl">
+      <Header category="Pie" title="Project Cost Breakdown" />
+      <div className="w-full">
+        <PieChart
+          id="chart-pie"
+          data={pieChartData}
+          legendVisiblity
+          background={currentMode === "dark" ? "#33373E" : "#fff"}
+          height="full"
+        />
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Pie;
